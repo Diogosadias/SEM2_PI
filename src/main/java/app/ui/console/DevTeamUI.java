16c4f9786081234d276f7df05ef3dacf30ae30f9
Márcio Ramos <1201682@isep.ipp.pt>
2021-04-24 16:53:56 +0100

    DevTeamUI.java with the development team names and emails

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -3,6 +3,7 @@ package app.ui.console;
 /**
  *
  * @author Paulo Maio <pam@isep.ipp.pt>
+ * @author Márcio Ramos <1201682@isep.ipp.pt>
  */
 public class DevTeamUI implements Runnable{
 
@@ -14,10 +15,14 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t Bruno Miguel Moreira Pereira - 1191454@isep.ipp.pt \n");
+        System.out.printf("\t Diogo Sá Oliveira Pereira Dias - 1161605@isep.ipp.pt \n");
+        System.out.printf("\t Vasco Barcelos Santos Sodre - 1150920@isep.ipp.pt \n");
+        System.out.printf("\t Tiago José de Almeida Ferreira - 1200601@isep.ipp.pt \n");
+        System.out.printf("\t Tomás Afonso Rodrigues Matos Pinto - 1181835@isep.ipp.pt \n");
+        System.out.printf("\t Márcio Rodrigues Ramos - 1201682@isep.ipp.pt \n");
+        System.out.printf("\t Tiago José Tomaz da Rocha - 1181445@isep.ipp.pt \n");
+        System.out.printf("\t Gil Pimenta Pereira - 1180838@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
