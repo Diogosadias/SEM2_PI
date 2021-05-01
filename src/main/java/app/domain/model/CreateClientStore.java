9ff78f06faa8c773d7dd5cb217c674ed6c20989d
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-01 11:51:12 +0100

    update 01/05/2021 11:51

---
+++ src/main/java/app/domain/model/CreateClientStore.java
@@ -0,0 +1,15 @@
+package app.domain.model;
+
+public class CreateClientStore {
+    private Client newclient;
+    public CreateClientStore(){
+        this.newclient=newclient;
+    }
+    public void saveClient(){
+
+    }
+    public void validateClient(Client rc){
+
+    }
+
+}
