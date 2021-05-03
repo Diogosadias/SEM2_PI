23477312590e6b2691446ca27ed777d57e782d94
1191454 <1191454@isep.ipp.pt>
2021-05-03 20:41:26 +0100

    [US07][Code Implementation] Some modifications on the store class, and started implementing UI and Controller

--- src/main/java/app/ui/console/AdminUI.java
+++ src/main/java/app/ui/console/AdminUI.java
@@ -31,6 +31,8 @@ public class AdminUI implements Runnable{
         options.add(new MenuItem("Search types of test", new ShowTextUI("You have chosen option D")));
         options.add(new MenuItem("Delete type of test", new ShowTextUI("You have chosen option E")));
         options.add(new MenuItem("New Parameter Category", new CreateNewParameterUI()));
+        options.add(new MenuItem("Register employee", new RegisterEmployeeUI()));
+
 
         int option = 0;
         do
