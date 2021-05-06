48a53102b355c4e0ed81a5a2cf62a251e2d6a934
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-05-06 14:32:34 +0100

    CALTest

---
+++ src/test/java/app/domain/model/CALTest.java
@@ -0,0 +1,14 @@
+package app.domain.model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class CALTest extends TestCase {
+
+    @Test
+    public void testGetAddress() {
+        CAL cal = new CAL(111,"hello",1111,"adress",1000,true);
+
+        assertEquals("adress",cal.getAddress());
+    }
+}
