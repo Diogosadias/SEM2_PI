bd28e99d5122c2a7d911be11901590a27db8da39
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-05-06 20:25:08 +0100

    Revert "Solving yellow"
    This reverts commit 7c34b310af223e075786e90467e6ed062b820652.

---
+++ src/test/java/app/test/CreateClientStoreTest.java
@@ -0,0 +1,17 @@
+package app.test;
+
+
+import auth.AuthFacade;
+import auth.domain.model.Email;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CreateClientStoreTest extends TestCase {
+
+
+}
