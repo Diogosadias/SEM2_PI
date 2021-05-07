05feb6ca29660cea4d02d3933428ad0ecb0a2354
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-07 10:08:53 +0100

    Changes added to classes "SpecifyNewParameterUI". [UC-10][Implementation]

--- src/main/java/app/ui/console/SpecifyNewParameterUI.java
+++ src/main/java/app/ui/console/SpecifyNewParameterUI.java
@@ -3,6 +3,7 @@ package app.ui.console;
 import app.controller.ParameterCategoryController;
 import app.controller.ParameterController;
 import app.domain.model.Parameter;
+import app.domain.model.ParameterCategory;
 
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ public class SpecifyNewParameterUI implements Runnable{
     public SpecifyNewParameterUI() {
 
     }
-    public void run(){
+    public void run() {
 
         String code;
         String name;
@@ -25,35 +26,40 @@ public class SpecifyNewParameterUI implements Runnable{
         do {
             System.out.println("New code (5 alphanumeric characters): ");
             code = read.next().trim();
-        }while(code.length()>5|| code.equals(null));
+        } while (code.length() > 5 || code.equals(null));
         do {
             System.out.println("Name (max. 8 characters): ");
             name = read.next().trim();
-        }while(name.length()>5 || name.equals(null));
+        } while (name.length() > 5 || name.equals(null));
         do {
             System.out.println("Description (max. 20 characters): ");
             description = read.next().trim();
-        }while(description.length()>20 ||name.equals(null));
+        } while (description.length() > 20 || name.equals(null));
 
         //fazer trim() dos nomes, e tambem verificar se name tem numeros ou se code tem caracteres especiais....(extras)
+        ParameterCategory codeparameter;
+        do {
+            System.out.println("\nSelect parameter category: ");
+            ParameterCategoryController pcc = new ParameterCategoryController();
+            pcc.writeParameterCategories();
 
 
-        System.out.println("\nSelect parameter category: ");
-        ParameterCategoryController pc = new ParameterCategoryController();
-        pc.writeParameterCategories();
+            System.out.print("\n->");
+            categoryselect = read.next();
 
-
-        System.out.print("\n->");
-        categoryselect = read.next();
-
+            codeparameter = pcc.getParameterCategory(categoryselect);
+            if (codeparameter == null) {
+                System.out.println("Parameter category doesn't exist!");
+            }
+        }while(codeparameter==null);
         ParameterController p = new ParameterController();
-        boolean created = p.createParameter(code,name,description,categoryselect);
+        boolean created = p.createParameter(code, name, description, categoryselect);
 
-        if(created){
+        if (created) {
             System.out.println("Parameter Created!");
-        }
-        else {
+        } else {
             System.out.println("Unsuccesfully created the Parameter");
         }
+
     }
 }
