52cf032db29004aae4af2b62a256cb281bdfc06c
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-08 18:06:19 +0100

    Added test "SpecifyNewTestStoreTest" [UC-09][Unit Testing]

---
+++ src/test/java/app/domain/model/SpecifyNewTestStoreTest.java
@@ -0,0 +1,35 @@
+package app.domain.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class SpecifyNewTestStoreTest {
+
+    @Test
+    public void createTestType() {
+    }
+
+    @Test
+    public void validateTestType() {
+    }
+
+    @Test
+    public void saveTestType() {
+    }
+
+    @Test
+    public void deleteTestType() {
+    }
+
+    @Test
+    public void searchTestType() {
+    }
+
+    @Test
+    public void getTestTypeByCode() {
+    }
+
+    @Test
+    public void getTestTypeList() {
+    }
+}
