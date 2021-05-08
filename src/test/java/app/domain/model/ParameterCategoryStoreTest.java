9a58383902765d10424f3544ea78dd2d2ddbae4e
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-08 17:34:00 +0100

    Test implementation to ParameterCategoryStore. Changes added to classes "ParameterCategoryStoreTest". Added test "ParameterCategoryStoreTest" [UC-11][Unit Testing]

---
+++ src/test/java/app/domain/model/ParameterCategoryStoreTest.java
@@ -0,0 +1,72 @@
+package app.domain.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ParameterCategoryStoreTest {
+    ParameterCategoryStore storeTest = new ParameterCategoryStore();
+    ParameterCategory p1 = new ParameterCategory("aaaaa","aaaaaaaa","aaaaa");
+
+    @Test
+    public void createParameterCategoryExistent() {
+        storeTest.saveParameterCategory(p1);
+        boolean b1 = storeTest.validateParameterCategory(p1);
+        assertEquals(false,b1);
+    }
+    @Test
+    public void createParameterCategory() {
+        //calculations
+        boolean b = storeTest.validateParameterCategory(p1);
+        assertEquals(true,b);
+    }
+
+    @Test
+    public void getParameterCategoryByCode() {
+        boolean b2 = storeTest.validateParameterCategory(null);
+        assertFalse(b2);
+    }
+
+    @Test
+    public void validateParameterCategory() {
+    }
+
+    @Test
+    public void saveParameterCategory() {
+        boolean b = storeTest.saveParameterCategory(p1);
+        assertEquals(true,b);
+
+        boolean b1 = storeTest.saveParameterCategory(p1);
+        assertEquals(false,b1);
+    }
+
+    @Test
+    public void getParameterCategoryList() {
+        ParameterCategory p2 = storeTest.createParameterCategory("aaaa2","aaaaaaaa","aaaaa");
+        storeTest.saveParameterCategory(p1);
+        storeTest.saveParameterCategory(p2);
+        List<ParameterCategory> storeTestList = new ArrayList<>();
+        storeTestList.add(p1);
+        storeTestList.add(p2);
+
+        assertEquals(storeTestList, storeTest.getParameterCategoryList());
+    }
+    @Test
+    public void testGetNotNullParameterCategoryList() {
+        ParameterCategoryStore storetest2 = new ParameterCategoryStore();
+        assertNotNull(storetest2.getParameterCategoryList());
+    }
+
+    @Test
+    public void testToString() {
+        ParameterCategory p2 = new ParameterCategory("aaaa2","aaaaaaaa","aaaaa");
+
+        storeTest.saveParameterCategory(p1);
+        storeTest.saveParameterCategory(p2);
+        String check = "ParameterCategory{code='aaaaa', description='aaaaaaaa', nhsId='aaaaa'}\nParameterCategory{code='aaaa2', description='aaaaaaaa', nhsId='aaaaa'}\n";
+        assertEquals(check, storeTest.toString());
+    }
+}
