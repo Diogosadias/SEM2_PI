1ff743e17e28f29351b979acc5b8b61cf2303ce9
Gil Pereira (1180838) <1180838@isep.ipp.pt>
2021-05-08 18:07:46 +0100

    [Testing] SpecialistDoctor Update

--- src/test/java/app/domain/model/SpecialistDoctorTest.java
+++ src/test/java/app/domain/model/SpecialistDoctorTest.java
@@ -24,10 +24,25 @@ public class SpecialistDoctorTest extends TestCase {
     }
 
     @Test
-    public void testTestEquals() {
+    public void testEquals() {
         SpecialistDoctor testDoc2 = new SpecialistDoctor(testRole, "TT000001", "name", "address",
                 12345678900L, "1234567890", 1234567890);
         assertTrue(testDoc.equals(testDoc2));
-        testDoc2.setDoctorIndexNumber(1234567888);
+    }
+    @Test
+    public void testEqualsForDifferent() {
+        SpecialistDoctor testDoc2 = new SpecialistDoctor(testRole, "TT005001", "tiago tocha", "address",
+                12345678900L, "1234567890", 1234567890);
+        assertFalse(testDoc.equals(testDoc2));
+    }
+    @Test
+    public void testEqualsForDifferentInstances() {
+        String test = new String();
+        assertFalse(testDoc.equals(test));
+    }
+    @Test
+    public void testTestEqualsForSuperClass() {
+        Employee test = new Employee(new OrgRole("12345","safdxfasf"), "1111", "John", "Address", 12345678912L,"12345" );
+        assertFalse(testDoc.equals(test));
     }
 }
