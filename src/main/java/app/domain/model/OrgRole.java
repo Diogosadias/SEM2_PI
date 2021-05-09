30e226d6f7eb71a61b795e036ccd2952d128a46f
1181445 <1181445@isep.ipp.pt>
2021-05-09 01:22:15 +0100

    us7 classes updated

--- src/main/java/app/domain/model/OrgRole.java
+++ src/main/java/app/domain/model/OrgRole.java
@@ -40,12 +40,11 @@ public class OrgRole {
 
     public Employee createEmployee (EmployeeDto dto) {
         //Later changing to Reflection technique
-        Employee employee = new Employee(this, dto.getEmployeeId(), dto.getName(), dto.getAddress(), dto.getPhoneNumber(), dto.getSocCode());
-        if(this.getDesignation() == "SpecialistDoctor") {
-           return new SpecialistDoctor(this, dto.getEmployeeId(), dto.getName(), dto.getAddress(), dto.getPhoneNumber(), dto.getSocCode(), dto.getDoctorIndexNumber());
+        Employee employee = new Employee(this, dto);
+        if(this.getDesignation().equals("SpecialistDoctor")) {
+            return new SpecialistDoctor(employee, dto.getDoctorIndexNumber());
         }
         return employee;
-
     }
 
     @Override
