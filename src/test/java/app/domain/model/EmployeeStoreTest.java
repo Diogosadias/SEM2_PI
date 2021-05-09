d0c5387575a071adb436b6dc17d6434e65a76745
1191454 <1191454@isep.ipp.pt>
2021-05-09 14:32:39 +0100

    Added most tests of EmployeeStoreTests

--- src/test/java/app/domain/model/EmployeeStoreTest.java
+++ src/test/java/app/domain/model/EmployeeStoreTest.java
@@ -1,61 +1,105 @@
 package app.domain.model;
 
-import auth.domain.model.Email;
-import org.junit.Assert;
+import app.domain.dto.EmployeeDto;
+import static app.domain.shared.Constants.ADMINISTRATOR;
+import static app.domain.shared.Constants.CHEMISTRY_TECHNOLOGIST;
+import static app.domain.shared.Constants.LABORATORY_COORDINATOR;
+import static app.domain.shared.Constants.MEDICAL_LAB_TECHNICIAN;
+import static app.domain.shared.Constants.RECEPTIONIST;
+import static app.domain.shared.Constants.SPECIALIST_DOCTOR;
+import auth.AuthFacade;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.*;
 
+/**
+ *
+ * @author Bruno Pereira
+ */
 public class EmployeeStoreTest  {
     Employee e1 = new Employee(new OrgRole("12345","safdxfasf"), "1111", "John", "Address", 12345678912L,"12345" );
-    EmployeeStore es = new EmployeeStore();
-
+    EmployeeDto dto = new EmployeeDto("teste", "teste", "teste", 12345678912L, "12335");
+    EmployeeStore es = new EmployeeStore();  
+    
     @Test
-    public void testAddOrgRoles() {
+    public void testGetOrgRoles() {        
+        System.out.println("GetOrgRoles");
+        List<OrgRole> expResult = new ArrayList();
+        expResult.add(new OrgRole("role_1",SPECIALIST_DOCTOR));
+        expResult.add(new OrgRole("role_2",LABORATORY_COORDINATOR));
+        expResult.add(new OrgRole("role_3",CHEMISTRY_TECHNOLOGIST));
+        expResult.add(new OrgRole("role_4",MEDICAL_LAB_TECHNICIAN));
+        expResult.add(new OrgRole("role_5",RECEPTIONIST));
+        expResult.add(new OrgRole("role_6",ADMINISTRATOR));
+        List<OrgRole> result = es.getOrgRoles();
+        assertEquals(expResult, result);      
+
+
     }
 
-    @Test
-    public void testGetOrgRoles() {
-        List<OrgRole> lor = new ArrayList<>();
-        assertNull(null);
-        try{
-            es.setLor(lor);
-        } catch (IllegalArgumentException ex) {
-            assertEquals("Organization Roles list is empty.", ex.getMessage());
-        }
-
-
-    }
-
-    @Test
-    public void testRegisterEmployee() {
-    }
+//    @Test
+//    public void testRegisterEmployee() {        
+//        System.out.println("RegisterEmployee");
+//        Employee expResult = new Employee(new OrgRole("teste","teste"),new EmployeeDto("teste", "teste", "teste", 12345678900L, "12335"));
+//        Employee result = es.registerEmployee(dto);
+//        assertEquals(expResult, result); 
+//    }
 
     @Test
     public void testGenerateEmployeeId() {
+        System.out.println("GenerateEmployeeId");
+        String expResult = es.generateEmployeeId(e1.getName());
+        String result = "J00001";
+        assertEquals(expResult, result);    
     }
 
     @Test
-    public void testGetNumEmployees() {
+    public void testGetNumEmployees() {     
+        System.out.println("GetNumEmployees");   
+        int expResult = 0;
+        int result = es.getNumEmployees();
+        assertEquals(expResult, result);    
     }
 
     @Test
-    public void testGetRoleById() {
+    public void testGetRoleById() {         
+        System.out.println("GetRoleById");
+        OrgRole expResult = new OrgRole("role_5",RECEPTIONIST);
+        OrgRole result = es.getRoleById("role_5");        
+        assertEquals(expResult , result);  
     }
 
     @Test
-    public void testValidateEmployee() {
+    public void testValidateEmployee() {                
+        System.out.println("ValidateEmployee");
+        boolean b = es.validateEmployee(e1);
+        assertEquals(true,b);
+        es.saveEmployee(e1);
+        boolean b1 = es.validateEmployee(e1);
+        assertEquals(false,b1);
+        boolean b2 = es.validateEmployee(e1);
+        assertFalse(b2);
     }
 
     @Test
-    public void testSaveEmployee() {
+    public void testSaveEmployee() {       
+        System.out.println("SaveEmployee");    
+        boolean b = es.saveEmployee(e1);
+        assertEquals(true,b);
     }
 
-    @Test
-    public void testGetEmployees() {
-    }
+//    @Test
+//    public void testGetEmployees() {        
+//        System.out.println("GetEmployees");              
+//        Employee temp = es.registerEmployee(new EmployeeDto("teste2", "teste2", "teste2", 12345678900L, "72612"));
+//        es.saveEmployee(temp);
+//        List<Employee> result = new ArrayList<>();
+//        result.add(temp);        
+//        List<Employee> expResult = es.getEmployees();
+//        assertEquals(expResult, result);
+//    }
 }
