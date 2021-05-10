44367354401e5823be36507cbfdcc8c6a7bd89a7
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-10 17:35:29 +0100

    aaaa

--- src/test/java/app/domain/model/CreateClientStoreTest.java
+++ src/test/java/app/domain/model/CreateClientStoreTest.java
@@ -12,14 +12,34 @@ public class CreateClientStoreTest {
 
     @Test
     public void testValidateClient() {
+<<<<<<< HEAD
+=======
+        //testing
+        boolean b = tccs.validateClient(c1);
+        assertEquals(true,b);
+        tccs.saveClient(c1,"asdf");
+        boolean b1 = tccs.validateClient(c1);
+        assertEquals(false,b1);
+        boolean b2 = tccs.validateClient(null);
+        assertFalse(b2);
+>>>>>>> parent of 3dafb5a (fixing errors and adding javadoc coments)
     }
 
     @Test
     public void testSaveClient() {
+<<<<<<< HEAD
     }
 
     @Test
     public void setClientList() {
+=======
+        //testing
+        boolean b = tccs.saveClient(c1,"asdf");
+        assertEquals(true,b);
+
+        boolean b1 = tccs.saveClient(c1,"asdf");
+        assertEquals(false,b1);
+>>>>>>> parent of 3dafb5a (fixing errors and adding javadoc coments)
     }
 
     @Test
