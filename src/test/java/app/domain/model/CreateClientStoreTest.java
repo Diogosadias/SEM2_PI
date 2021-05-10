93da624f49f82fcef93b28a3b9f09b4986e75408
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-10 17:07:01 +0100

    bug fixing 1.2

--- src/test/java/app/domain/model/CreateClientStoreTest.java
+++ src/test/java/app/domain/model/CreateClientStoreTest.java
@@ -1,71 +1,28 @@
 package app.domain.model;
 
-import auth.AuthFacade;
-import auth.domain.model.Email;
 import org.junit.Test;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class CreateClientStoreTest {
-   /* AuthFacade auth = new AuthFacade();
-
-    Client c1 = new Client(new Email("usedafr1@gmail.com"), "John", 1111111111L, 1111111111111111L, 111111111111L, new Date("2001/12/22"), "M", 11111111111L);
-    CreateClientStore tccs = new CreateClientStore(auth);
-
 
     @Test
-    public void testCreateClient() {
-        //arrange
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("2021-12-12");
-
-        //calculations
-        Client c2 = tccs.createClient("usxedar1@gmail.com", "John", 1111111111L, 1111111111111111L, 111111111111L, "2001/12/22", "M", 11111111111L);
-
-        //assert
-        assertEquals(c1.getPNumber(),c2.getPNumber());
-        assertEquals(c1.getCitizenCard(),c2.getCitizenCard());
-        assertEquals(c1.getNhs(),c2.getNhs());
-        assertEquals(c1.getTin(),c2.getTin());
-        assertEquals(c1.getSex(),c2.getSex());
+    public void createClient() {
     }
 
     @Test
-    public void testValidateClient() {
-        //testing
-        boolean b = tccs.validateClient(c1);
-        assertEquals(true,b);
-        tccs.saveClient(c1,"asdfaaaaaaa");
-        boolean b1 = tccs.validateClient(c1);
-        assertEquals(false,b1);
-        boolean b2 = tccs.validateClient(null);
-        assertFalse(b2);
+    public void validateClient() {
     }
 
     @Test
-    public void testSaveClient() {
-        //testing
-        boolean b = tccs.saveClient(c1,"asdfaaaaaaa");
-        assertEquals(true,b);
-
-        boolean b1 = tccs.saveClient(c1,"asdfaaaaaaa");
-        assertEquals(false,b1);
+    public void saveClient() {
     }
 
     @Test
-    public void testGetClientList() {
-        AuthFacade auth = null;
-        CreateClientStore ccs = new CreateClientStore(auth);
-        Client c1 = new Client(new Email("usewdar1@gmail.com"), "John", 1111111111L, 1111111111111111L, 111111111111L, new Date("2001/12/22"), "M", 11111111111L);
-        List<Client> testList = new ArrayList<>();
-        //calculations
-        ccs.setClientList(c1);
-        testList.add(c1);
-        //assemble
-        assertEquals(testList,ccs.getClientList());
-    }*/
+    public void setClientList() {
+    }
+
+    @Test
+    public void getClientList() {
+    }
 }
