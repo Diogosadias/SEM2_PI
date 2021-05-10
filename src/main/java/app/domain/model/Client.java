3ff1d9c97e955bb8ffd880d5a749e9088c3dd48e
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-10 19:01:41 +0100

    bug fixing 2.0

--- src/main/java/app/domain/model/Client.java
+++ src/main/java/app/domain/model/Client.java
@@ -298,8 +298,8 @@ public class Client {
      */
     public void checkPNumber(long phonenumber){
         String temp = String.valueOf(phonenumber);
-        if ( temp.length() <11) {
-            throw new IllegalArgumentException("Phone Number code must have 11 chars.");
+        if (temp.length() !=11) {
+            throw new IllegalArgumentException("Phone Number must have 11 chars.");
         }
     }
 
