3c5eb5ecbe708e9909c1eb378d894a4b8eb0f4a3
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-18 15:42:28 +0100

    EmailSender improvement!

--- src/main/java/app/domain/shared/EmailSender.java
+++ src/main/java/app/domain/shared/EmailSender.java
@@ -1,5 +1,6 @@
 package app.domain.shared;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,19 +11,28 @@ public class EmailSender {
     }
 
     private void sendEmail (String email, String pass){
+        BufferedWriter myWriter = null;
         try{
-            FileWriter myWriter = new FileWriter(email +".txt");
+            myWriter = new BufferedWriter(new FileWriter(email +".txt"));
             myWriter.write("Hello,\nhere is your new password:\n\n");
             myWriter.append("Email: " + email + "\n");
             myWriter.append("Password: " + pass + "\n");
             myWriter.append("\nBest regards\n");
             myWriter.append("ManyLabs team.");
             System.out.println("Sending your new password to your email...");
-            myWriter.close();
+
         }catch (IOException e){
             e.printStackTrace();
         }finally{
-            System.out.println("Email with new password sent!");
+            try{
+                if (myWriter!=null) {
+                    System.out.println("Email with new password sent!");
+                    myWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
