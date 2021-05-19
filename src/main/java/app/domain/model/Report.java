cdd8cb9bcc1bff54ee11bb9daa2a5901d8b64c89
Diogo Dias <1161605@isep.ipp.pt>
2021-05-19 12:33:19 +0100

    [UC-14] Coding

--- src/main/java/app/domain/model/Report.java
+++ src/main/java/app/domain/model/Report.java
@@ -1,5 +1,9 @@
 package app.domain.model;
 
+import org.apache.tools.ant.util.Tokenizer;
+
+import java.util.StringTokenizer;
+
 public class Report {
     private String diagnosis;
     private Test test;
@@ -7,9 +11,38 @@ public class Report {
 
 
     Report (String diagnosis, Test test){
-    this.diagnosis = diagnosis;
-    this.test = test;
+        checkDiagnosis(diagnosis);
+        this.diagnosis = diagnosis;
+        this.test = test;
 
+    }
+
+    public String getDiagnosis(){
+        return diagnosis;
+    }
+
+
+    public Test getTest(){
+        return test;
+    }
+
+
+
+    public boolean checkDiagnosis(String diagnosis){
+        if(diagnosis!= null){
+            diagnosis.trim();
+            if(countwords(diagnosis)>400){
+                throw new IllegalArgumentException("Diagnosis has an Invalid Input! (>400 words)");
+            }
+            return true;
+        }
+        throw new IllegalArgumentException("Diagnosis has an Invalid Input!");
+    }
+
+    public int countwords(String s){
+        StringTokenizer st = new StringTokenizer(s);
+        int count = st.countTokens();
+        return count;
     }
 
 }
