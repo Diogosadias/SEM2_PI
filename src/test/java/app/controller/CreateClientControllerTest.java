66ab2f740fa37f674a1e78c18472d757aff4d111
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-22 18:36:17 +0100

    bug fixing 8000

--- src/test/java/app/controller/CreateClientControllerTest.java
+++ src/test/java/app/controller/CreateClientControllerTest.java
@@ -4,29 +4,30 @@ import app.domain.model.Client;
 import app.domain.model.ClientStore;
 import auth.AuthFacade;
 import auth.domain.model.Email;
-import junit.framework.TestCase;
-
+import org.junit.Test;
 import java.util.Date;
-
+import static org.junit.Assert.*;
 /**
  *
  * @author Márcio Ramos <1201682@isep.ipp.pt>
  */
-public class CreateClientControllerTest extends TestCase {
+public class CreateClientControllerTest{
 
+    @Test
     public void testCreateClient() {
 
         //arrange
         CreateClientController ccr = new CreateClientController();
 
         //calculating
-        boolean b1 = ccr.createClient("user1@gmail.com", "John", 1111111111, 1111111111111111l, 111111111111l, "12/12/2021", "M", 11111111111l);
+        boolean b1 = ccr.createClient("user2@gmail.com", "John", 1211111111, 1211111111111111l, 121111111111l, "12/12/2021", "M", 14411111111l);
 
         //assert
         assertEquals(true,b1);
 
     }
 
+    @Test
     public void testSaveClient() {
         //arange
         Client c1 = new Client(new Email("user1@gmail.com"), "John", 1111111111, 1111111111111111l, 111111111111l, new Date("2021/12/22"), "M", 11111111111l);
@@ -39,6 +40,7 @@ public class CreateClientControllerTest extends TestCase {
         assertEquals(true,b1);
     }
 
+    @Test
     public void testWriteClient() {
         //arrange
         AuthFacade auth = new AuthFacade();
