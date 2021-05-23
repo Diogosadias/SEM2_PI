d4cabda3d697554662d014a0f7ad5d25044e0190
Márcio Ramos <1201682@isep.ipp.pt>
2021-05-23 18:38:07 +0100

    bug fixing

--- src/main/java/app/controller/App.java
+++ src/main/java/app/controller/App.java
@@ -124,8 +124,7 @@ App {
 
         //Client
         Client client1 = new Client (new Email("client1@lei.pt"),"Client Teste",1234567890L,1212121212121212L,210000000001L,new Date("1990/01/01"),"M",91000000000L);
-        this.authFacade.addUserWithRole(client1.getName(), client1.getId().getEmail(), "123" , Constants.ROLE_CLIENT);
-        this.company.getClientStore().addClient(client1);
+        this.company.getClientStore().saveClients(client1,"123");
 
         //ParameterCategory - Covid Tests
         ParameterCategory categoryCovid = new ParameterCategory("CC-01","SARS-CoV-2","21001");
