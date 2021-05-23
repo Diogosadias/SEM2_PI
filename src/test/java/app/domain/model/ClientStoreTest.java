caf9c504a64d4f167aafb116a3ce4c1041f1636f
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-05-23 16:54:00 +0100

    .

--- src/test/java/app/domain/model/ClientStoreTest.java
+++ src/test/java/app/domain/model/ClientStoreTest.java
@@ -46,13 +46,12 @@ public class ClientStoreTest {
     @Test
     public void testSaveClient() {
         //testing
-        boolean b = store.validateClient(c1);
+        boolean b = store.saveClient(c1,"aaaa");
         assertEquals(true,b);
-        store.saveClient(c1,"aaaa");
-        boolean b1 = store.validateClient(c1);
+
+        Client c2 = new Client(new Email("usedafr1@gmail.com"), "John", 1111111111L, 1111111111111111L, 111111111111L, new Date("2001/12/22"), "M", 11111111111L);
+        boolean b1 = store.saveClient(c2,"aaaa");
         assertEquals(false,b1);
-        boolean b2 = store.validateClient(null);
-        assertFalse(b2);
     }
 
     @Test
