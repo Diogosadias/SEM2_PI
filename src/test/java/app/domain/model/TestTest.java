5017844b264da52625702d5810529509a0d9601d
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-05-28 15:13:25 +0100

    [Mutation] Test class validation

--- src/test/java/app/domain/model/TestTest.java
+++ src/test/java/app/domain/model/TestTest.java
@@ -13,18 +13,28 @@ public class TestTest {
     TestType testtype = new TestType("codex","description","collectingMethod");
     Client c1 = new Client(new Email("user1@gmail.com"), "John", 1111111111L, 1111111111111111L, 111111111111L, new Date("12/12/2021"), "M", 11111111111L);
 
-     app.domain.model.Test test = new app.domain.model.Test(testtype,"description",c1);
+    app.domain.model.Test test = new app.domain.model.Test(testtype,"description",c1);
 
-     @Test
+    @Test
     public void checkValidation(){
-         try{
-         app.domain.model.Test test1 = new app.domain.model.Test(null,"description",c1);
-         }catch(IllegalArgumentException ex){
-             assertEquals("Creating Test Error: Test type is null.",ex.getMessage());
-         }
+        try{
+            app.domain.model.Test test1 = new app.domain.model.Test(null,"description",c1);
+        }catch(IllegalArgumentException ex){
+            assertEquals("Creating Test Error: Test type is null.",ex.getMessage());
+        }
+        try{
+            app.domain.model.Test test1 = new app.domain.model.Test(testtype,"",c1);
+        }catch (IllegalArgumentException ex){
+            assertEquals("Creating Test Error: Description is empty.",ex.getMessage());
+        }
+        try{
+            app.domain.model.Test test1 = new app.domain.model.Test(testtype,"description",null);
+        }catch (IllegalArgumentException ex){
+            assertEquals("Creating Test Error: Client is null.",ex.getMessage());
+        }
 
 
-     }
+    }
 
 
 
