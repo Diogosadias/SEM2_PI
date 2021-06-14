cfa993035f21ad60f559a7e436f34709d095d86c
Gil Pereira (1180838) <1180838@isep.ipp.pt>
2021-06-14 11:45:55 +0100

    [Implementation] Med Lab Tech Dashboard and bug fixes

--- src/main/java/app/ui/gui/LoginGUI.java
+++ src/main/java/app/ui/gui/LoginGUI.java
@@ -139,7 +139,7 @@ public class LoginGUI implements Initializable, GuiMethods {
         rolesUI.add(new MenuItem(Constants.ROLE_RECEP, new ReceptionistUI()));
         rolesUI.add(new MenuItem(Constants.SPECIALIST_DOCTOR, new SpecialistDoctorUI()));
         rolesUI.add(new MenuItem(Constants.CHEMISTRY_TECHNOLOGIST, new ClinicalChemistryTechnologistUI()));
-        rolesUI.add(new MenuItem(Constants.MEDICAL_LAB_TECHNICIAN, new MedicalLabTechnicianUI()));
+        rolesUI.add(new MenuItem(Constants.MEDICAL_LAB_TECHNICIAN, "/fxml/MedLabTechnicianGUI.fxml"));
         rolesUI.add(new MenuItem(Constants.LABORATORY_COORDINATOR, new LabCoordinatorUI()));
         rolesUI.add(new MenuItem(Constants.ROLE_CLIENT, new ClientUI()));
 
---
+++ src/main/java/app/ui/gui/MedLabTechnicianGUI.java
@@ -0,0 +1,81 @@
+package app.ui.gui;
+
+import app.ui.Main;
+import app.ui.console.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MedLabTechnicianGUI implements Initializable, GuiMethods {
+    private Main mainInstance;
+
+    @FXML
+    private ListView<String> myListView;
+
+    int option;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        List<MenuItem> options = new ArrayList<>();
+
+        options.add(new MenuItem("Create Sample", new RecordSampleUI()));
+        options.add(new MenuItem("List Test Sample(s)", new ListSampleCollectedUI()));
+
+
+        for(int i=0; i<options.size();i++){
+            myListView.getItems().add(options.get(i).toString());
+        }
+
+        myListView.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                option = myListView.getSelectionModel().getSelectedIndex();
+
+                if(options.get(option).getGui()==null) {
+                    options.get(option).run();
+                }else{
+                    try {
+                        options.get(option).runGui(options.get(option).getGui(),mainInstance);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    @FXML
+    private void menu_goBack(ActionEvent event) {
+        try {
+            MenuItem item= new MenuItem("default", "/fxml/LoginGUI.fxml");
+            item.runGui(item.getGui(),mainInstance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void menu_exit(ActionEvent event) {
+        mainInstance.getStage().close();
+    }
+
+    @Override
+    public void setInstance(Main mainInstance) {
+        this.mainInstance=mainInstance;
+    }
+
+
+}
